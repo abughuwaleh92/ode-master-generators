ec849c059a08cc26499fbf40b26904d21ba5470c
abughuwaleh92 <engmhmd92@gmail.com>
2025-08-04 02:31:06 +0300

    Update init.py

--- pipeline/init.py
+++ pipeline/init.py
@@ -1,9 +1,1 @@
-"""Pipeline module for ODE Master Generators"""
 
-from .generator import ODEDatasetGenerator
-from .parallel import ParallelODEGenerator
-
-__all__ = [
-    'ODEDatasetGenerator',
-    'ParallelODEGenerator'
-]
